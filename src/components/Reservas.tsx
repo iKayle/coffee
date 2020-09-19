9219c4849996807c095c4dc105c9d03cb1668759
kaylebarrt <kayle.barrt@gmail.com>
2020-09-18 23:10:22 -0300

    fim

--- src/components/Reservas.tsx
+++ src/components/Reservas.tsx
@@ -7,22 +7,6 @@ export function Reservas() {
         </p>
         <p className="text-5xl">RESERVE UMA MESA</p>
       </div>
-      <div className="container flex flex-col justify-between mx-auto">
-        <p className="pb-10 text-lg tracking-wide text-gray-600">
-          Aqui é onde o seu texto começa. Basta clicar e começar a escrever.
-          Quae ab illo inventore veritatis et quasi architecto beatae vitae
-          dicta sunt explicabo nemo enim ipsam voluptatem quia voluptas sit
-          aspernatur aut odit aut fugit sed quia consequuntur magni.
-        </p>
-        <p className="flex pb-8 text-lg font-bold text-gray-600 merri">
-          Reservas: 55 81 3216 54 98
-        </p>
-        <div className="container flex justify-between h-full pt-4 mx-auto">
-          <button className="px-20 py-3 pt-4 text-xl tracking-wide border border-golden">
-            RESERVE UMA MESA
-          </button>
-        </div>
-      </div>
     </div>
   );
 }
--- src/pages/reservas.tsx
+++ src/pages/reservas.tsx
@@ -1,10 +1,45 @@
+import { Footer } from "../components/Footer";
 import { Menus } from "../components/Menus";
 
 export default function Reservas() {
   return (
     <div>
       <Menus />
-      <div></div>
+      <div className="container flex justify-between pt-32 mx-auto">
+        <div className="container flex mx-auto">
+          <form className="container flex flex-col w-auto">
+            <h2 className="text-gray-600 "> Nome</h2>
+            <input
+              className="px-40 py-3 text-gray-600 border border-golden"
+              name="name"
+            />
+            <h2 className="pt-8 text-gray-600 "> Data</h2>
+
+            <input
+              className="px-40 py-3 text-gray-600 border border-golden"
+              type="date"
+            />
+            <h2 className="pt-8 text-gray-600"> Mensagem</h2>
+            <textarea className="p-2 text-gray-600 border border-golden" />
+          </form>
+        </div>
+        <div className="container mx-auto ">
+          <h2 className="text-5xl allura text-golden">
+            Está afim de da uma passada ?
+          </h2>
+          <h1 className="pb-6 text-5xl"> RESERVE UMA MESA </h1>
+          <p className="pb-6 tracking-wide text-gray-600 text-md">
+            Aqui é onde o seu texto começa. Basta clicar e começar a escrever.
+            <br /> perspiciatis unde omnis iste natus error sit voluptatem
+            accusantium <br />
+            doloremque laudantium totam rem aperiam eaque ipsa.
+          </p>
+          <p className="pb-64 font-semibold text-gray-600 merri">
+            Reservas: 55 81 3216 54 98
+          </p>
+        </div>
+      </div>
+      <Footer />
     </div>
   );
 }
